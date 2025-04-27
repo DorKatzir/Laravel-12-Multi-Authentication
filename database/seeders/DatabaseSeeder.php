686212d67f689f10b9c29d6609c6e61cf8a49a79
Dor Katzir <katzirdor@gmail.com>
2025-04-27 13:55:08 +0300

    Admin Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use App\Models\Admin;
 
-
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -21,12 +18,5 @@ class DatabaseSeeder extends Seeder
             AdminSeeder::class,
         ]);
 
-
-        // User::factory(10)->create();
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
