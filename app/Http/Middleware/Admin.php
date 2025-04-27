712ba4f67b8533d865f0859fa5096e1234e2da58
Dor Katzir <katzirdor@gmail.com>
2025-04-27 23:43:34 +0300

    Middleware -Configure User and Admin logins

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Auth;
 
 class Admin
 {
@@ -15,6 +16,12 @@ class Admin
      */
     public function handle(Request $request, Closure $next): Response
     {
+        // Check if the user is authenticated and has the 'admin' role
+        if (!Auth::guard('admin')->check()) {
+            // Redirect to the login page if not authenticated
+            return redirect()->route('admin_login');
+        }
+
         return $next($request);
     }
 }
--- app/Http/Middleware/User.php
+++ app/Http/Middleware/User.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Auth;
 
 class User
 {
@@ -15,6 +16,11 @@ class User
      */
     public function handle(Request $request, Closure $next): Response
     {
+        // Check if the user is authenticated
+        if (!Auth::guard('web')->check()) {
+            // Redirect to the login page if not authenticated
+            return redirect()->route('login');
+        }
         return $next($request);
     }
 }
